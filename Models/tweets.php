b4873335ad78a1e2150f4594cd5f573d2e73987c
sora0606 <shimadasora1406@gmail.com>
2021-06-07 01:08:25 +0900

    ホーム機能実装

--- Models/tweets.php
+++ Models/tweets.php
@@ -36,3 +36,66 @@ function createTweet(array $data)
 
     return $response;
 }
+
+/**
+ * ツイート一覧を取得
+ *
+ * @param array $user ログインしているユーザー情報
+ * @return array|false ツイート一覧の配列｜false
+ */
+function findTweets(array $user)
+{
+    $mysqli= new mysqli(DB_HOST , DB_USER , DB_PASS , DB_NAME);
+    // 接続チェック
+    if( $mysqli -> connect_error ){
+        echo 'MySQLの接続に失敗しました。：'.$mysqli -> connect_error."\n";
+        exit;
+    }
+
+    // ログインユーザーIDをエスケープ
+    $login_user_id = $mysqli -> real_escape_string($user['id']);
+
+    // 検索のSQL文を作成
+    $query = <<< SQL
+        select
+            -- ツイートテーブルの取得したいカラムの指定
+            T.id as tweet_id,
+            T.status as tweet_status,
+            T.body as tweet_body,
+            T.image_name as tweet_image_name,
+            T.created_at as tweet_created_at,
+            -- ユーザーテーブルの取得したいカラムの指定
+            U.id as user_id,
+            U.name as user_name,
+            U.nickname as user_nickname,
+            U.image_name as user_image_name,
+            -- ログインユーザーがいいね！したか（している場合、値が入る）
+            L.id as like_id,
+            -- いいね！数
+            (select count(*) from likes where status = "active" and tweet_id = T.id) as like_count
+
+        from
+            tweets as T
+        join
+            -- ユーザーテーブルを紐づける
+            users as U on U.id = T.user_id and U.status = 'active'
+        left join
+            likes as L on L.tweet_id = T.id and L.status = 'active' and L.user_id = '$login_user_id'
+        where
+            T.status = 'active'
+
+    SQL;
+
+    // SQL実行
+    if($result = $mysqli -> query($query)){
+        // データを配列で受け取る
+        $response = $result -> fetch_all(MYSQLI_ASSOC);
+    }else{
+        $response = false;
+        echo 'エラーメッセージ： ' . $mysqli -> error . "\n";
+    }
+
+    $mysqli -> close();
+
+    return $response;
+}
